14ac7ad386794f4d32cbb603dcf56808d1fcd36c
durgeshvipul <durgeshpandey419@gmail.com>
2022-11-29 07:29:37 +0530

    Using two pointer method

---
+++ 100DaysOfCode/CyclicTwo.java
@@ -0,0 +1,15 @@
+public class CyclicTwo {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        int i=0, j=arr.length-1;
+        
+        while(i!=j){
+            int temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+        }
+        System.out.print(java.util.Arrays.toString(arr));
+    }
+}
